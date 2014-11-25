7d0cc29dd4d7bdb78f06f4245286c493ad164cf8
francesco-strazzullo <me@francescostrazzullo.info>
2014-11-25 23:39:55 +0100

    used kineticJs to write the canvas

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,29 +1,69 @@
 angular.module('weQuote.directives', [])
-.directive('quoteCanvas', [function() {
-	return {
-		restrict: 'E',
-   	 	template: '<div style="display:none;"><canvas width="500" height="500"></canvas></div>',
-    	replace: true,
-    	link: function ($scope, element, attrs) {
-    		var canvas = element[0].children[0];
-    		var context = canvas.getContext("2d");
+	.directive('quoteCanvas', [function() {
+		return {
+			restrict: 'E',
+			template: '<div style="display:none;"></div>',
+			replace: true,
+			link: function($scope, element, attrs) {
+				var id = attrs["id"];
 
-    		$scope.$on('generate-canvas',function(event,url,quote,callback){
-    			
-    			var image = new Image();
-				image.onload = function() {
-					context.drawImage(image, 0, 0);
+				if (!id) {
+					id = Math.random().toString(36).substring(8);
+					element.attr('id', id);
+				}
 
-					context.fillStyle = "white";
-					context.font = "52px Lobster";
-					context.fillText(quote.text, 100, 100);
-
-		    		callback(canvas.toDataURL());
+				var kinetic = {
+					stage: new Kinetic.Stage({
+						container: id,
+						width: 500,
+						height: 500
+					})
 				};
 
-				image.src = url;
-				
-    		});
-    	}
-	};
-}])
+				$scope.kinetic = kinetic;
+
+				$scope.$on('generate-canvas', function(event, url, quote, callback) {
+
+					$scope.kinetic.stage.clear();
+
+					var imageObj = new Image();
+
+					imageObj.onload = function() {
+						var layer = new Kinetic.Layer();
+
+						var image = new Kinetic.Image({
+							x: 0,
+							y: 0,
+							image: imageObj,
+							width: 500,
+							height: 500
+						});
+
+						var quoteText = new Kinetic.Text({
+					        x: 0,
+					        y: 0,
+					        text: quote.text,
+					        fontSize: 52,
+					        fontFamily: 'Lobster',
+					        fill: '#FFFFFF',
+					        width: 480,
+					        padding: 20,
+					        align: 'center'
+					      });
+
+						layer.add(image);
+						layer.add(quoteText);
+
+						$scope.kinetic.stage.add(layer);
+						$scope.kinetic.stage.toDataURL({
+							callback: callback
+						});
+
+					};
+					
+					imageObj.src = url;
+
+				});
+			}
+		};
+	}])
