ba3b36458391612b51ac057b29d2caf9d6c28634
francesco-strazzullo <me@francescostrazzullo.info>
2014-11-26 21:01:34 +0100

    added author text

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,5 +1,60 @@
 angular.module('weQuote.directives', [])
-	.directive('quoteCanvas', [function() {
+	.directive('quoteCanvas', ['$log',function($log) {
+
+		var that = this;
+
+		var IMG_SIZE = 500;
+
+		this.getQuoteText = function(quote){
+			var quoteText = new Kinetic.Text(
+				{
+					text: quote.text,
+					fontSize: 52,
+					fontFamily: 'Lobster',
+					fill: '#FFFFFF',
+					width: IMG_SIZE,
+					padding: 0,
+					align: 'center'
+				});
+
+			//Y center
+			var textHeight = quoteText.getAttr('height');
+			$log.debug(textHeight);
+			quoteText.setAttr('y',(IMG_SIZE - textHeight)/2);
+
+			return quoteText;
+		};
+
+		this.getAuthorText = function(quote){
+			var autorText = new Kinetic.Text(
+				{
+					text: quote.author,
+					fontSize: 24,
+					fontFamily: 'Lobster',
+					fill: '#FFFFFF',
+					width: IMG_SIZE,
+					padding: 0,
+					align: 'center'
+				});
+
+			//Stick to bottom
+			var textHeight = autorText.getAttr('height');
+			autorText.setAttr('y',IMG_SIZE - textHeight -25);
+
+			return autorText;
+		};
+
+		this.getImageBackgroud = function(imgElement){
+			return new Kinetic.Image(
+				{
+					x: 0,
+					y: 0,
+					image: imgElement,
+					width: IMG_SIZE,
+					height: IMG_SIZE
+				});
+		}
+
 		return {
 			restrict: 'E',
 			template: '<div style="display:none;"></div>',
@@ -31,28 +86,9 @@ angular.module('weQuote.directives', [])
 					imageObj.onload = function() {
 						var layer = new Kinetic.Layer();
 
-						var image = new Kinetic.Image({
-							x: 0,
-							y: 0,
-							image: imageObj,
-							width: 500,
-							height: 500
-						});
-
-						var quoteText = new Kinetic.Text({
-					        x: 0,
-					        y: 0,
-					        text: quote.text,
-					        fontSize: 52,
-					        fontFamily: 'Lobster',
-					        fill: '#FFFFFF',
-					        width: 480,
-					        padding: 20,
-					        align: 'center'
-					      });
-
-						layer.add(image);
-						layer.add(quoteText);
+						layer.add(that.getImageBackgroud(imageObj));
+						layer.add(that.getAuthorText(quote));
+						layer.add(that.getQuoteText(quote));
 
 						$scope.kinetic.stage.add(layer);
 						$scope.kinetic.stage.toDataURL({
