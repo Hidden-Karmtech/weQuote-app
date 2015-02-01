b53729e6a2ba4781e6052de45bc62082d3a8497b
francesco-strazzullo <me@francescostrazzullo.info>
2015-02-01 23:38:55 +0100

    moved card generation to a service

--- www/js/directives.js
+++ www/js/directives.js
@@ -10,19 +10,9 @@ angular.module('weQuote.directives', [])
 			}
 		};
 	}])
-	.directive('quoteCard', ['$log', 'CardSize', function($log, CardSize) {
+	.directive('quoteCard', ['$log', 'CardGenerator', function($log,CardGenerator) {
 
 		var that = this;
-
-		var TEXT_SCALE_FACTOR = 0.9;
-		var TEXT_X_OFFSET = 3;
-		var WATERMARK_OFFSET = 3;
-		var AUTHOR_OFFSET = 7;
-		var WATERMARK_FONT_SIZE = 4.5;
-		var AUTHOR_FONT_SIZE = 7;
-		var BORDER_WIDTH = 3.5;
-		var HEIGHT_THRESHOLD = 60;
-		var START_FONT_SIZE = 10;
 
 		var visibleId = Math.random().toString(36).substring(8);
 		var hiddenId = Math.random().toString(36).substring(8);
@@ -30,144 +20,6 @@ angular.module('weQuote.directives', [])
 		var visibleKinetic;
 		var invisibleKinetic;
 		var count = 0;
-
-		this.getSize = CardSize.getSize;
-
-		var EmptyCard = function(canvasId,size){
-
-			var strokeWidth = size * (BORDER_WIDTH / 100);
-
-			var stage = new Kinetic.Stage({
-				container: canvasId,
-				width: size,
-				height: size
-			});
-
-			var	layer = new Kinetic.Layer();
-
-			var textCanvas = new Kinetic.Text({
-				fontFamily: 'Lobster',
-				padding: 0,
-				align: 'center'
-			});
-
-			var watermark = new Kinetic.Text({
-				text: 'wequote.it',
-				fontFamily: 'Lobster',
-				padding: 0,
-				align: 'left'
-			});
-
-
-			var	authorText = new Kinetic.Text({
-				fontFamily: 'Lobster',
-				padding: 0,
-				align: 'center'
-			});
-			
-			var rectBorder = new Kinetic.Image({
-				x: 0,
-				y: 0,
-				width: size,
-				height: size,
-				fillAlpha: 0,
-				stroke: '#98A4D7',
-				strokeWidth: strokeWidth
-			});
-
-			var imageBackground = new Kinetic.Image({
-				x: 0,
-				y: 0,
-				width: size,
-				height: size
-			});
-
-			layer.add(imageBackground);
-			layer.add(rectBorder);
-			layer.add(authorText);
-			layer.add(textCanvas);
-			layer.add(watermark);
-
-			stage.add(layer);
-
-			return {
-				stage:stage,
-				mainLayer:layer,
-				textCanvas:textCanvas,
-				imageBackground:imageBackground,
-				rectBorder:rectBorder,
-				authorText:authorText,
-				watermark:watermark,
-				size:size
-			};
-		};
-
-		this.printQuoteText = function(area, quote) {
-
-			var startFontSize = area.size * (START_FONT_SIZE / 100);
-			var xOffset = area.size * (TEXT_X_OFFSET / 100);
-			var fontColor = quote.fontColor || '#FFFFFF';
-
-			var printText = function(text, fontSize, size) {
-				
-				area.textCanvas.text(text);
-				area.textCanvas.fontSize(fontSize);
-				area.textCanvas.fill(fontColor);
-				area.textCanvas.x(xOffset);
-				area.textCanvas.width(size - (xOffset * 2));
-
-			};
-
-			var HeightThreshold = area.size * (HEIGHT_THRESHOLD / 100);
-			var fontSize;
-			var textHeight;
-
-			do {
-				$log.debug("Generating text with font " + fontSize);
-
-				fontSize = fontSize ? (fontSize * TEXT_SCALE_FACTOR) : (startFontSize || 36);
-				printText(quote.text, fontSize, area.size);
-				textHeight = area.textCanvas.getAttr('height');
-
-				$log.debug("text height " + textHeight);
-			} while (textHeight > HeightThreshold)
-
-			//Y center
-			area.textCanvas.setAttr('y', (area.size - textHeight) / 2);
-		};
-
-		this.printWatermark = function(area, quote) {
-
-			var watermark = area.watermark;
-			var offset = area.size * (WATERMARK_OFFSET / 100);
-			var fontSize = area.size * (WATERMARK_FONT_SIZE / 100);
-			var fontColor = quote.fontColor || '#FFFFFF';
-
-			watermark.fontSize(fontSize);
-			watermark.x(offset);
-			watermark.y(offset);
-			watermark.fill(fontColor);
-			watermark.width(area.size);
-		};
-
-		this.printAuthorText = function(area, quote) {
-
-			var authorText = area.authorText;
-			var fontSize = area.size * (AUTHOR_FONT_SIZE / 100);
-			var fontColor = quote.fontColor || '#FFFFFF';
-
-			authorText.text(quote.author);
-			authorText.fontSize(fontSize);
-			authorText.fill(fontColor);
-			authorText.width(area.size);
-
-			//Stick to bottom
-
-			var yOffset = area.size * (AUTHOR_OFFSET / 100);
-
-			var textHeight = authorText.getAttr('height');
-			authorText.y(area.size - textHeight - yOffset);
-		};
 
 		var generateCard = function(kineticArea, quote, startFontSize, callback) {
 
@@ -183,7 +35,7 @@ angular.module('weQuote.directives', [])
 					that.cropImage(imageObj);
 				} else {
 
-					that.paintCanvas(kineticArea, imageObj, quote, startFontSize);
+					CardGenerator.updateCard(kineticArea, imageObj, quote, startFontSize);
 
 					if (callback) {
 						stage.toDataURL({
@@ -249,18 +101,6 @@ angular.module('weQuote.directives', [])
 			imageObj.src = canvas.toDataURL();
 		};
 
-		//kineticArea, imageObj, quote, startFontSize
-		this.paintCanvas = function(area,imageObj, quote, startFontSize) {
-
-			area.imageBackground.image(imageObj);
-			that.printAuthorText(area,quote);
-			that.printQuoteText(area,quote);
-			that.printWatermark(area,quote);
-
-			area.stage.draw();
-
-		};
-
 		return {
 			restrict: 'E',
 			template: '<div><div style="z-index:10000" id=' + visibleId + '></div><div style="display:none" id=' + hiddenId + '></div><canvas style="display:none"id=' + canvasId + '></div>',
@@ -272,8 +112,8 @@ angular.module('weQuote.directives', [])
 
 				$log.debug("Linking directive");
 
-				visibleKinetic = new EmptyCard(visibleId,that.getSize());
-				invisibleKinetic = new EmptyCard(hiddenId,1000);
+				visibleKinetic = CardGenerator.generateEmptyCard(visibleId);
+				invisibleKinetic = CardGenerator.generateEmptyCard(hiddenId,1000);
 				
 				$scope.$watch('quote', function(quote) {
 					if (quote && quote.url) {
--- www/js/services.js
+++ www/js/services.js
@@ -24,6 +24,165 @@ angular.module('weQuote.services', [])
 			}
 		}
 	}])
+	.service('CardGenerator', ['$log','CardSize', function($log,CardSize) {
+		
+		var TEXT_SCALE_FACTOR = 0.9;
+		var TEXT_X_OFFSET = 3;
+		var WATERMARK_OFFSET = 3;
+		var AUTHOR_OFFSET = 7;
+		var WATERMARK_FONT_SIZE = 4.5;
+		var AUTHOR_FONT_SIZE = 7;
+		var BORDER_WIDTH = 3.5;
+		var HEIGHT_THRESHOLD = 60;
+		var START_FONT_SIZE = 10;
+
+		var printQuoteText = function(area, quote) {
+
+			var startFontSize = area.size * (START_FONT_SIZE / 100);
+			var xOffset = area.size * (TEXT_X_OFFSET / 100);
+			var fontColor = quote.fontColor || '#FFFFFF';
+
+			var printText = function(text, fontSize, size) {
+				
+				area.textCanvas.text(text);
+				area.textCanvas.fontSize(fontSize);
+				area.textCanvas.fill(fontColor);
+				area.textCanvas.x(xOffset);
+				area.textCanvas.width(size - (xOffset * 2));
+
+			};
+
+			var HeightThreshold = area.size * (HEIGHT_THRESHOLD / 100);
+			var fontSize;
+			var textHeight;
+
+			do {
+				fontSize = fontSize ? (fontSize * TEXT_SCALE_FACTOR) : (startFontSize || 36);
+				printText(quote.text, fontSize, area.size);
+				textHeight = area.textCanvas.getAttr('height');
+
+				$log.debug("Font Ratio: " + (quote.text.length / fontSize));
+			} while (textHeight > HeightThreshold)
+
+			//Y center
+			area.textCanvas.setAttr('y', (area.size - textHeight) / 2);
+		};
+
+		var printWatermark = function(area, quote) {
+
+			var watermark = area.watermark;
+			var offset = area.size * (WATERMARK_OFFSET / 100);
+			var fontSize = area.size * (WATERMARK_FONT_SIZE / 100);
+			var fontColor = quote.fontColor || '#FFFFFF';
+
+			watermark.fontSize(fontSize);
+			watermark.x(offset);
+			watermark.y(offset);
+			watermark.fill(fontColor);
+			watermark.width(area.size);
+		};
+
+		var printAuthorText = function(area, quote) {
+
+			var authorText = area.authorText;
+			var fontSize = area.size * (AUTHOR_FONT_SIZE / 100);
+			var fontColor = quote.fontColor || '#FFFFFF';
+
+			authorText.text(quote.author);
+			authorText.fontSize(fontSize);
+			authorText.fill(fontColor);
+			authorText.width(area.size);
+
+			//Stick to bottom
+
+			var yOffset = area.size * (AUTHOR_OFFSET / 100);
+
+			var textHeight = authorText.getAttr('height');
+			authorText.y(area.size - textHeight - yOffset);
+		};
+
+		return {
+			generateEmptyCard: function(containerId, size) {
+
+				var size = size || CardSize.getSize();
+
+				var strokeWidth = size * (BORDER_WIDTH / 100);
+
+				var stage = new Kinetic.Stage({
+					container: containerId,
+					width: size,
+					height: size
+				});
+
+				var layer = new Kinetic.Layer();
+
+				var textCanvas = new Kinetic.Text({
+					fontFamily: 'Lobster',
+					padding: 0,
+					align: 'center'
+				});
+
+				var watermark = new Kinetic.Text({
+					text: 'wequote.it',
+					fontFamily: 'Lobster',
+					padding: 0,
+					align: 'left'
+				});
+
+
+				var authorText = new Kinetic.Text({
+					fontFamily: 'Lobster',
+					padding: 0,
+					align: 'center'
+				});
+
+				var rectBorder = new Kinetic.Image({
+					x: 0,
+					y: 0,
+					width: size,
+					height: size,
+					fillAlpha: 0,
+					stroke: '#98A4D7',
+					strokeWidth: strokeWidth
+				});
+
+				var imageBackground = new Kinetic.Image({
+					x: 0,
+					y: 0,
+					width: size,
+					height: size
+				});
+
+				layer.add(imageBackground);
+				layer.add(rectBorder);
+				layer.add(authorText);
+				layer.add(textCanvas);
+				layer.add(watermark);
+
+				stage.add(layer);
+
+				return {
+					stage: stage,
+					mainLayer: layer,
+					textCanvas: textCanvas,
+					imageBackground: imageBackground,
+					rectBorder: rectBorder,
+					authorText: authorText,
+					watermark: watermark,
+					size: size
+				};
+			},
+			updateCard: function(area,imageObj, quote, startFontSize) {
+				area.imageBackground.image(imageObj);
+				
+				printAuthorText(area,quote);
+				printQuoteText(area,quote);
+				printWatermark(area,quote);
+
+				area.stage.draw();
+			}
+		}
+	}])
 	.service('OfflineData', ['$http', '$log', function($http, $log) {
 		return {
 			get: function() {
@@ -51,23 +210,23 @@ angular.module('weQuote.services', [])
 			var validFiles = [];
 			var i = 0;
 
-			var validTags = _.filter(quote.tags,function(tag){
+			var validTags = _.filter(quote.tags, function(tag) {
 				return Backgrounds[tag.name];
-			}); 
+			});
 
-			if(validTags.length){
-				validTags = _.map(validTags,function(tag){
+			if (validTags.length) {
+				validTags = _.map(validTags, function(tag) {
 					return tag.name;
 				});
-			}else{
+			} else {
 				validTags = ['misc'];
 			}
 
-			_.each(validTags,function(tag){
+			_.each(validTags, function(tag) {
 				var count = Backgrounds[tag];
 
-				if(count){
-					for(i=0;i<count;i++){
+				if (count) {
+					for (i = 0; i < count; i++) {
 						validFiles.push(tag + '/' + _.str.pad(i, 3, '0') + '.jpg');
 					}
 				}
